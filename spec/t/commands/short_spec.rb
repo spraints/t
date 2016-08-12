77e1f4dd28c02f8ae2e9eb7a905fd1b3239812a4
Matt Burke <spraints@gmail.com>
2016-08-12 12:06:14 -0400

    add a spark graph to `t short`

--- spec/t/commands/short_spec.rb
+++ spec/t/commands/short_spec.rb
@@ -4,7 +4,7 @@ require 'support/command_helpers'
 require 't/commands/short'
 
 describe T::Commands::Short do
-  subject(:command) { described_class.new(:out => stdout, :file => t_file, :sparks => %W(0 1 2 3 4 5 6)) }
+  subject(:command) { described_class.new(:out => stdout, :file => t_file, :step => 30) }
   include CommandHelpers
 
   context 'with no file' do
@@ -39,21 +39,21 @@ E_T
     end
 
     it { command.run; expect(stdout.string).to eq(<<SHORT) }
-2013-07-28 - 2013-08-03       0/  2
-2013-08-04 - 2013-08-10       0/  0
-2013-08-11 - 2013-08-17       0/  1
-2013-08-18 - 2013-08-24       0/  1
-2013-08-25 - 2013-08-31       0/  1
-2013-09-01 - 2013-09-07       4/  5
+2013-07-28 - 2013-08-03     0/  2  (    0/   59 minutes)  ||
+2013-08-04 - 2013-08-10     0/  0  (    0/    0 minutes)  
+2013-08-11 - 2013-08-17     0/  1  (    0/   61 minutes)  |||
+2013-08-18 - 2013-08-24     0/  1  (    0/   62 minutes)  |||
+2013-08-25 - 2013-08-31     0/  1  (    0/   63 minutes)  |||
+2013-09-01 - 2013-09-07     4/  5  (   45/   64 minutes)  ..|
 SHORT
 
     it { command.run("60"); expect(stdout.string).to eq(<<SHORT) }
-2013-07-28 - 2013-08-03       2/  2
-2013-08-04 - 2013-08-10       0/  0
-2013-08-11 - 2013-08-17       0/  1
-2013-08-18 - 2013-08-24       0/  1
-2013-08-25 - 2013-08-31       0/  1
-2013-09-01 - 2013-09-07       5/  5
+2013-07-28 - 2013-08-03     2/  2  (   59/   59 minutes)  ..
+2013-08-04 - 2013-08-10     0/  0  (    0/    0 minutes)  
+2013-08-11 - 2013-08-17     0/  1  (    0/   61 minutes)  |||
+2013-08-18 - 2013-08-24     0/  1  (    0/   62 minutes)  |||
+2013-08-25 - 2013-08-31     0/  1  (    0/   63 minutes)  |||
+2013-09-01 - 2013-09-07     5/  5  (   64/   64 minutes)  ...
 SHORT
 
   end
