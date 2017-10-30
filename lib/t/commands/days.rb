f5e9661036db6648e517bf984477fb451e84d9c6
Matt Burke <spraints@gmail.com>
2017-10-30 06:45:10 -0400

    show monthly and annual totals

--- lib/t/commands/days.rb
+++ lib/t/commands/days.rb
@@ -16,34 +16,137 @@ module T
 
       def run
         data = Data.new(@file)
+        current_month = nil
+        current_year = nil
         each_week(data) do |week_start, week_end, entries|
-          cols = []
-          cols << [week_start, week_end-1].map { |d| d.strftime(T::DATE_FORMAT) }.join(" - ")
-          total = 0
-          (0..6).each do |dow|
+          unless in?(current_year, week_start)
+            show current_year
+            current_year = Year.new(week_start)
+          end
+          unless in?(current_month, week_start)
+            show current_month
+            current_month = Month.new(week_start)
+          end
+          current_week = Week.new(week_start)
+
+          min_per_day = (0..6).map { |dow|
             day_start = week_start + dow * ONE_DAY
             day_end   = day_start + ONE_DAY
-            day_minutes = entries.inject(0) { |n, e| n + e.minutes_between(day_start, day_end) }
-            cols.push(day_minutes == 0 ? "   " : ("%3d" % day_minutes))
-            total += day_minutes
-          end
-          cols << ("%5d" % total)
-          @stdout.puts cols.join(" | ")
+            entries.inject(0) { |n, e| n + e.minutes_between(day_start, day_end) }
+          }
+          current_week << min_per_day
+          current_month << min_per_day
+          current_year << min_per_day
+
+          show current_week
         end
+        show current_month
+        show current_year
+#          cols = []
+#          cols << [week_start, week_end-1].map { |d| d.strftime(T::DATE_FORMAT) }.join(" - ")
+#          total = 0
+#          (0..6).each do |dow|
+#            day_minutes = entries.inject(0) { |n, e| n + e.minutes_between(day_start, day_end) }
+#            cols.push(day_minutes == 0 ? "   " : ("%3d" % day_minutes))
+#            total += day_minutes
+#          end
+#          cols << ("%5d" % total)
+#          @stdout.puts cols.join(" | ")
       end
 
       private
 
       ONE_DAY = 60 * 60 * 24 # seconds
 
-      def minutes_for_day(day_start, entries)
-        day_end = day_start + ONE_DAY
-        min = entries.inject(0) { |n, e| n + e.minutes_between(day_start, day_end) }
-        min == 0 ? "   " : ("%3d" % min)
+      def in?(period, day)
+        period && period.include?(day)
+      end
+
+      def show(period)
+        if period
+          total = 0
+          days = period.days.map { |min| total += min; min == 0 ? "   " : ("%3d" % min) }.join(" | ")
+          @stdout.printf "%-23s | %s | %5d\n", period.label, days, total
+        end
       end
 
       def each_week(data, &block)
         T::Util::WeekGrouping.new(data).each_week(&block)
+      end
+
+      class Week
+        def initialize(day)
+          @start = day
+        end
+
+        def label
+          "%s - %s" % [@start.strftime(T::DATE_FORMAT), (@start + 6 * ONE_DAY).strftime(T::DATE_FORMAT)]
+        end
+
+        def <<(min_per_day)
+          @min_per_day = min_per_day
+        end
+
+        def days
+          @min_per_day
+        end
+      end
+
+      class AccumulateMinutes
+        def <<(min_per_day)
+          @min_per_day ||= [0]*7
+          @min_per_day = @min_per_day.zip(min_per_day).map { |a,b| a + b }
+        end
+
+        def days
+          @min_per_day
+        end
+      end
+
+      class Month
+        def initialize(day)
+          @year = day.year
+          @month = day.month
+        end
+
+        def label
+          "%4d-%02d" % [@year, @month]
+        end
+
+        def include?(day)
+          @year == day.year &&
+            @month == day.month
+        end
+
+        def <<(x)
+          (@accum ||= AccumulateMinutes.new) << x
+        end
+
+        def days
+          @accum.days
+        end
+      end
+
+      class Year
+        def initialize(day)
+          @year = day.year
+        end
+
+        def label
+          "%4d" % @year
+        end
+
+        def include?(day)
+          @year == day.year
+        end
+
+        def <<(x)
+          (@accum ||= AccumulateMinutes.new) << x
+        end
+
+        def days
+          @accum.days
+        end
       end
     end
   end
--- spec/t/commands/days_spec.rb
+++ spec/t/commands/days_spec.rb
@@ -42,14 +42,15 @@ E_T
 
     let(:lines) { stdout.string.split("\n") }
     it { expect(lines[0]).to  eq "2013-07-28 - 2013-08-03 |     |     |     |     |  30 |  29 |     |    59" }
-    it { expect(lines[1]).to  eq "2013-08-04 - 2013-08-10 |     |     |     |     |     |     |     |     0" }
-    it { expect(lines[2]).to  eq "2013-08-11 - 2013-08-17 |  61 |     |     |     |     |     |     |    61" }
-    it { expect(lines[3]).to  eq "2013-08-18 - 2013-08-24 |     |     |     |     |  62 |     |     |    62" }
-    it { expect(lines[4]).to  eq "2013-08-25 - 2013-08-31 |     |     |     |     |     |     |  63 |    63" }
-    skip { expect(lines[5]).to  eq "2013-08                 |  61 |     |     |     |  30 |  29 |     |    59" }
-    skip { expect(lines[6]).to  eq "2013-09-01 - 2013-09-07 |     |     |     |     |     |     |     |     0" }
-    skip { expect(lines[7]).to  eq "2013-09                 |     |     |     |     |     |     |     |     0" }
-    skip { expect(lines[8]).to  eq "2013                    |     |     |     |     |     |     |     |     0" }
-    skip { expect(lines[9]).to  be_nil }
+    it { expect(lines[1]).to  eq "2013-07                 |     |     |     |     |  30 |  29 |     |    59" }
+    it { expect(lines[2]).to  eq "2013-08-04 - 2013-08-10 |     |     |     |     |     |     |     |     0" }
+    it { expect(lines[3]).to  eq "2013-08-11 - 2013-08-17 |  61 |     |     |     |     |     |     |    61" }
+    it { expect(lines[4]).to  eq "2013-08-18 - 2013-08-24 |     |     |     |     |  62 |     |     |    62" }
+    it { expect(lines[5]).to  eq "2013-08-25 - 2013-08-31 |     |     |     |     |     |     |  63 |    63" }
+    it { expect(lines[6]).to  eq "2013-08                 |  61 |     |     |     |  62 |     |  63 |   186" }
+    it { expect(lines[7]).to  eq "2013-09-01 - 2013-09-07 |     |     |     |  84 |  23 |     |     |   107" }
+    it { expect(lines[8]).to  eq "2013-09                 |     |     |     |  84 |  23 |     |     |   107" }
+    it { expect(lines[9]).to  eq "2013                    |  61 |     |     |  84 | 115 |  29 |  63 |   352" }
+    it { expect(lines[10]).to be_nil }
   end
 end
