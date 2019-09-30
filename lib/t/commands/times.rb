c4a119dba400a72c54c2836081a5e50c3d8723c4
Matt Burke <spraints@gmail.com>
2019-09-30 12:16:16 -0400

    Add 't times'

---
+++ lib/t/commands/times.rb
@@ -0,0 +1,143 @@
+require 't'
+require 't/data'
+require 't/util/week_grouping'
+
+module T
+  module Commands
+    class Times
+      def initialize(options = {})
+        @stdout           = options.fetch(:out) { $stdout }
+        @file             = options.fetch(:file) { T::DATA_FILE }
+      end
+
+      def legend_type
+        :week
+      end
+
+      def run
+        data = Data.new(@file)
+        current_month = nil
+        current_year = nil
+        each_week(data) do |week_start, week_end, entries|
+          unless in?(current_year, week_start)
+            show current_year
+            current_year = Year.new(week_start)
+          end
+          unless in?(current_month, week_start)
+            show current_month
+            current_month = Month.new(week_start)
+          end
+          current_week = Week.new(week_start)
+
+          entry_starts = entries.map(&:start_time)
+          start_per_day = (0..6).map { |dow|
+            day_start = week_start + dow * ONE_DAY
+            day_end   = day_start + ONE_DAY
+            entry_starts.select { |t| t >= day_start && t < day_end }.min
+          }
+          current_week << start_per_day
+          current_month << start_per_day
+          current_year << start_per_day
+
+          show current_week
+        end
+        show current_month
+        show current_year
+      end
+
+      private
+
+      ONE_DAY = 60 * 60 * 24 # seconds
+
+      def in?(period, day)
+        period && period.include?(day)
+      end
+
+      def show(period)
+        if period
+          days = period.days.map { |t| t.nil? ? "     " : t.strftime("%H:%M") }.join(" | ")
+          @stdout.printf "%-23s || %s |\n", period.label, days
+        end
+      end
+
+      def each_week(data, &block)
+        T::Util::WeekGrouping.new(data).each_week(&block)
+      end
+
+      class Week
+        def initialize(day)
+          @start = day
+        end
+
+        def label
+          "%s - %s" % [@start.strftime(T::DATE_FORMAT), (@start + 6 * ONE_DAY).strftime(T::DATE_FORMAT)]
+        end
+
+        def <<(starts_per_day)
+          @starts_per_day = starts_per_day
+        end
+
+        def days
+          @starts_per_day
+        end
+      end
+
+      class AccumulateStarts
+        def <<(starts_per_day)
+          @starts_per_day ||= [nil]*7
+          @starts_per_day = @starts_per_day.zip(starts_per_day).map { |a,b| a.nil? ? b : (b.nil? ? a : (a < b ? a : b)) }
+        end
+
+        def days
+          @starts_per_day
+        end
+      end
+
+      class Month
+        def initialize(day)
+          @year = day.year
+          @month = day.month
+        end
+
+        def label
+          "%4d-%02d" % [@year, @month]
+        end
+
+        def include?(day)
+          @year == day.year &&
+            @month == day.month
+        end
+
+        def <<(x)
+          (@accum ||= AccumulateStarts.new) << x
+        end
+
+        def days
+          @accum.days
+        end
+      end
+
+      class Year
+        def initialize(day)
+          @year = day.year
+        end
+
+        def label
+          "%4d" % @year
+        end
+
+        def include?(day)
+          @year == day.year
+        end
+
+        def <<(x)
+          (@accum ||= AccumulateStarts.new) << x
+        end
+
+        def days
+          @accum.days
+        end
+      end
+    end
+  end
+end
---
+++ spec/t/commands/times_spec.rb
@@ -0,0 +1,89 @@
+require "spec_helper"
+require "support/command_helpers"
+
+require "t/commands/times"
+
+describe T::Commands::Times do
+  subject(:command) { described_class.new(:out => stdout, :file => t_file) }
+  include CommandHelpers
+
+  context "with no file" do
+    before do
+      File.unlink t_file
+      command.run
+    end
+    it { expect(stdout.string).to eq("") }
+  end
+
+  context 'with an empty file' do
+    before do
+      command.run
+    end
+    it { expect(stdout.string).to eq("") }
+  end
+
+  context 'with some entries in the file' do
+    before do
+      File.write(t_file, <<E_T)
+2013-08-01 10:45,2013-08-01 11:15
+2013-08-02 10:15,2013-08-02 10:44
+2013-08-11 10:45,2013-08-11 11:46
+2013-08-22 10:45,2013-08-22 11:47
+2013-08-31 10:45,2013-08-31 11:48
+2013-09-04 10:45,2013-09-04 11:04
+2013-09-04 11:04,2013-09-04 11:16
+2013-09-04 11:16,2013-09-04 11:26
+2013-09-04 11:16,2013-09-04 11:59
+2013-09-05 11:26,2013-09-05 11:39
+2013-09-05 11:39,2013-09-05 11:49
+E_T
+      command.run
+    end
+
+    let(:lines) { stdout.string.split("\n") }
+    it { expect(lines[0]).to  eq "2013-07-28 - 2013-08-03 ||       |       |       |       | 10:45 | 10:15 |       |" }
+    it { expect(lines[1]).to  eq "2013-07                 ||       |       |       |       | 10:45 | 10:15 |       |" }
+    it { expect(lines[2]).to  eq "2013-08-04 - 2013-08-10 ||       |       |       |       |       |       |       |" }
+    it { expect(lines[3]).to  eq "2013-08-11 - 2013-08-17 || 10:45 |       |       |       |       |       |       |" }
+    it { expect(lines[4]).to  eq "2013-08-18 - 2013-08-24 ||       |       |       |       | 10:45 |       |       |" }
+    it { expect(lines[5]).to  eq "2013-08-25 - 2013-08-31 ||       |       |       |       |       |       | 10:45 |" }
+    it { expect(lines[6]).to  eq "2013-08                 || 10:45 |       |       |       | 10:45 |       | 10:45 |" }
+    it { expect(lines[7]).to  eq "2013-09-01 - 2013-09-07 ||       |       |       | 10:45 | 11:26 |       |       |" }
+    it { expect(lines[8]).to  eq "2013-09                 ||       |       |       | 10:45 | 11:26 |       |       |" }
+    it { expect(lines[9]).to  eq "2013                    || 10:45 |       |       | 10:45 | 10:45 | 10:15 | 10:45 |" }
+    it { expect(lines[10]).to be_nil }
+  end
+
+#  # this won't work right in all time zones. :(
+#  context 'with some mixed time zone entries in the file' do
+#    before do
+#      File.write(t_file, <<E_T)
+#2013-08-01 10:45 -0400,2013-08-01 10:15 -0500
+#2013-08-02 08:15 -0600,2013-08-02 07:44 -0700
+#2013-08-11 10:45 -0400,2013-08-11 11:46 -0400
+#2013-08-22 10:45 -0400,2013-08-22 11:47 -0400
+#2013-08-31 10:45 -0400,2013-08-31 11:48 -0400
+#2013-09-04 10:45 -0400,2013-09-04 11:04 -0400
+#2013-09-04 11:04 -0400,2013-09-04 11:16 -0400
+#2013-09-04 11:16 -0400,2013-09-04 11:26 -0400
+#2013-09-04 11:16 -0400,2013-09-04 11:59 -0400
+#2013-09-05 11:26 -0400,2013-09-05 11:39 -0400
+#2013-09-05 11:39 -0400,2013-09-05 11:49 -0400
+#E_T
+#      command.run
+#    end
+#
+#    let(:lines) { stdout.string.split("\n") }
+#    it { expect(lines[0]).to  eq "2013-07-28 - 2013-08-03 ||       |       |       |       |    30 |    29 |       ||     59" }
+#    it { expect(lines[1]).to  eq "2013-07                 ||       |       |       |       |    30 |    29 |       ||     59" }
+#    it { expect(lines[2]).to  eq "2013-08-04 - 2013-08-10 ||       |       |       |       |       |       |       ||      0" }
+#    it { expect(lines[3]).to  eq "2013-08-11 - 2013-08-17 ||    61 |       |       |       |       |       |       ||     61" }
+#    it { expect(lines[4]).to  eq "2013-08-18 - 2013-08-24 ||       |       |       |       |    62 |       |       ||     62" }
+#    it { expect(lines[5]).to  eq "2013-08-25 - 2013-08-31 ||       |       |       |       |       |       |    63 ||     63" }
+#    it { expect(lines[6]).to  eq "2013-08                 ||    61 |       |       |       |    62 |       |    63 ||    186" }
+#    it { expect(lines[7]).to  eq "2013-09-01 - 2013-09-07 ||       |       |       |    84 |    23 |       |       ||    107" }
+#    it { expect(lines[8]).to  eq "2013-09                 ||       |       |       |    84 |    23 |       |       ||    107" }
+#    it { expect(lines[9]).to  eq "2013                    ||    61 |       |       |    84 |   115 |    29 |    63 ||    352" }
+#    it { expect(lines[10]).to be_nil }
+#  end
+end
