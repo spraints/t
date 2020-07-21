89bbfb2f49b9db9ec4362aea1eddb564f6ac4624
Matt Burke <spraints@gmail.com>
2020-07-21 08:34:42 -0400

    directly create the Time objects
    Using tempfile
    Using tempfile
    Rehearsal ----------------------------------------------------------------------------
    Time.parse                                 0.358570   0.001866   0.360436 (  0.360535)
    tp                                         0.233345   0.000432   0.233777 (  0.233900)
    ------------------------------------------------------------------- total: 0.594213sec
    
                                                   user     system      total        real
    Time.parse                                 0.389812   0.001644   0.391456 (  0.391525)
    tp                                         0.230507   0.000370   0.230877 (  0.230911)
    [2020-01-01 12:34:00 -0700, 2020-02-02 12:34:00 -0500, 20000]
    [2020-01-01 12:34:00 -0700, 2020-02-02 12:34:00 -0500, 20000]

--- bm.rb
+++ bm.rb
@@ -10,6 +10,9 @@ require "stringio"
 require "tempfile"
 require "time"
 
+USE_STRINGIO = ARGV[0] == "--stringio"
+USE_ARG = !ARGV[0].nil? && File.exist?(ARGV[0])
+
 N = 10000
 EXAMPLE = "2020-01-01 12:34 -0700\n2020-02-02 12:34\n" * N
 
@@ -22,45 +25,12 @@ def tp(io)
   #until io.eof?
     #c = io.read(1)
     state, data, entry = state.call(c, data)
-    res << pe(entry) if entry
+    res << entry if entry
   end
-  res << pe(data) if data
+  res << Time.new(*data) if data
   res
 end
 
-F = {:mon => 1, :day => 2, :hr => 3, :min => 4}
-def pe(data)
-  args = [nil, nil, nil, nil, nil, 0, nil]
-  while data
-    c, data = data
-    case c
-    when :tz
-      _, data = data
-      _, data = data
-      h2, data = data
-      h1, data = data
-      sign, data = data
-      tz = (D.fetch(h1)*10 + D.fetch(h2)) * 3600
-      tz = -tz if sign == "-"
-      args[6] = tz
-    when :year
-      y4, data = data
-      y3, data = data
-      y2, data = data
-      y1, data = data
-      args[0] = D.fetch(y1)*1000 + D.fetch(y2)*100 + D.fetch(y3)*10 + D.fetch(y4)
-    when :mon, :day, :hr, :min
-      d2, data = data
-      d1, data = data
-      args[F.fetch(c)] = D.fetch(d1)*10 + D.fetch(d2)
-    else
-      raise "Unexpected #{c.inspect} (#{data.inspect}) (#{args.inspect})"
-    end
-  end
-  Time.new(*args)
-end
-
-DIGITS = %w[0 1 2 3 4 5 6 7 8 9]
 D = {
   "0" => 0,
   "1" => 1,
@@ -74,10 +44,21 @@ D = {
   "9" => 9,
 }
 
+DIGITS = D.keys
+
+F = {
+  :year => 0,
+  :mon => 1,
+  :day => 2,
+  :hr => 3,
+  :min => 4,
+  :tz => 6,
+}
+
 Init = ->(c, _) do
   case c
   when *DIGITS
-    [Year, c, nil]
+    [Year, [D.fetch(c), 0, 0, 0, 0, 0, nil], nil]
   else
     [Init, nil, nil]
   end
@@ -86,6 +67,7 @@ end
 class DigitConsumer
   def initialize(label, len, next_char, next_state)
     @label = label
+    @index = F.fetch(label)
     @len = len
     @next_char = next_char
     @next_state = next_state
@@ -94,62 +76,100 @@ class DigitConsumer
   def call(c, data)
     case c
     when *DIGITS
-      [self, [c, data], nil]
+      data[@index] = 10*data[@index] + D.fetch(c)
+      [self, data, nil]
     when @next_char
-      [@next_state, [@label, data], nil]
-      #if data.first.size != @len
-      #  raise "error: #{@label} expected #{@len} digits, got #{data.first.size} (#{data.inspect})"
-      #else
-        #(prog = data[1] || []) << [@label, data[0]]
-        #[@next_state, [[], prog], nil]
-      #end
+      [@next_state, data, nil]
     else
       raise "error: #{@label} expected digit or #{@next_char.inspect}, got #{c.inspect} (#{data.inspect})"
     end
   end
 end
 
+MinuteIndex = F.fetch(:min)
+
 Minute = ->(c, data) do
   case c
   when *DIGITS
-    [Minute, [c, data], nil]
+    data[MinuteIndex] = 10*data[MinuteIndex] + D.fetch(c)
+    [Minute, data, nil]
   when " "
-    [TZPrefix, [:min, data], nil]
-  when "\n"
-    [Init, nil, [:min, data]]
-  #when " ", "\n"
-    #if data.first.size != 2
-    #  raise "error: min expected 2 digits, got #{data.first.size} (#{data.inspect})"
-    #else
-      #(prog = data[1] || []) << [:min, data[0]]
-      #[c == " " ? TZPrefix : FinishEntry, [[], prog], nil]
-    #end
+    [TZPrefix, data, nil]
+  when "\n", ","
+    [Init, nil, Time.new(*data)]
   else
     raise "error: min expected digit or #{" ".inspect} or #{"\n".inspect}, got #{c.inspect} (#{data.inspect})"
   end
 end
 
-FinishEntry = ->(c, data) do
-  s, d, _ = Init.call(c, nil)
-  [s, d, data]
-end
+TZIndex = F.fetch(:tz)
 
-TZ = DigitConsumer.new(:tz, 5, "\n", FinishEntry)
 TZPrefix = ->(c, data) do
   case c
-  when "+", "-"
-    [TZ, [c, data], nil]
+  when "+"
+    data[TZIndex] = 0
+    [PosTZ, data, nil]
+  when "-"
+    data[TZIndex] = 0
+    [NegTZ, data, nil]
   else
     raise "error: tz expected + or -, got #{c.inspect} (#{data.inspect})"
   end
 end
+
+class TZ
+  def self.new(sign)
+    stack = TZFinish.new(sign)
+    stack = TZDigit.new(10*60, stack)
+    stack = TZDigit.new(60*60, stack)
+    stack = TZDigit.new(10*60*60, stack)
+  end
+
+  class TZFinish
+    def initialize(sign)
+      @sign = sign
+    end
+
+    def call(c, data)
+      data[TZIndex] += D.fetch(c) * 60
+      data[TZIndex] *= @sign
+      [Init, nil, Time.new(*data)]
+    end
+  end
+
+  class TZDigit
+    def initialize(fact, stack)
+      @fact = fact
+      @stack = stack
+    end
+
+    def call(c, data)
+      data[TZIndex] += D.fetch(c) * @fact
+      [@stack, data, nil]
+    end
+  end
+end
+
+NegTZ = TZ.new(-1)
+PosTZ = TZ.new(1)
 Hour = DigitConsumer.new(:hr, 2, ":", Minute)
 Day = DigitConsumer.new(:day, 2, " ", Hour)
 Month = DigitConsumer.new(:mon, 2, "-", Day)
 Year = DigitConsumer.new(:year, 4, "-", Month)
 
+$first = true
 def mkio
-  #return StringIO.new(EXAMPLE)
+  if USE_STRINGIO
+    puts "Using stringio" if $first
+    $first = false
+    return StringIO.new(EXAMPLE)
+  end
+  if USE_ARG
+    puts "Using #{ARGV[0]}" if $first
+    $first = false
+    return File.open(ARGV[0])
+  end
+  puts "Using tempfile"
   f = Tempfile.new('t-benchmarks')
   f.write(EXAMPLE)
   f.rewind
@@ -160,11 +180,15 @@ control = candidate = nil
 candidateio = mkio
 controlio = mkio
 
-Benchmark.bm(40) do |x|
-  x.report("Array append") { (100*N).times { arr = []; 5.times { |i| arr << i } } }
-  x.report("Hash append") { (100*N).times { hash = {}; [:a, :b, :c, :d, :e].each { |x| hash[x] = x } } }
-  x.report("Time.parse") { controlio.rewind; control = controlio.readlines.map { |l| Time.parse(l) } }
-  x.report("tp") { candidateio.rewind; candidate = tp(candidateio) }
+Benchmark.bmbm(40) do |x|
+  x.report("Time.parse") do
+    controlio.rewind
+    control = controlio.readlines.flat_map { |l| l.split(",").map { |t| Time.parse(t) } }
+  end
+  x.report("tp") do
+    candidateio.rewind
+    candidate = tp(candidateio)
+  end
 end
 
 p [control.first, control.last, control.size]
