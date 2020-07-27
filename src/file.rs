02bdc01dbed95446fbc4e2828493a8ea3244e3fd
Matt Burke <spraints@gmail.com>
2020-07-27 07:30:10 -0400

    everything goes in Parser, add next failing test

--- src/file.rs
+++ src/file.rs
@@ -34,7 +34,7 @@ pub fn parse_entries(r: impl Read) -> Result<Vec<Entry>, Box<dyn Error>> {
     let mut parser = Parser::new(r);
     let mut res = vec![];
     loop {
-        match parse_entry(&mut parser)? {
+        match parser.parse_entry()? {
             None => break,
             Some(entry) => res.push(entry),
         }
@@ -57,6 +57,120 @@ impl<R: Read> Parser<R> {
         }
     }
 
+    fn parse_entry(&mut self) -> Result<Option<Entry>, Box<dyn Error>> {
+        match self.parse_time()? {
+            None => Ok(None),
+            Some((start, true)) => Ok(Some(Entry {
+                start: start,
+                stop: None,
+            })),
+            Some((start, false)) => match (self.parse_time())? {
+                None => Ok(Some(Entry {
+                    start: start,
+                    stop: None,
+                })),
+                Some((stop, _)) => Ok(Some(Entry {
+                    start: start,
+                    stop: Some(stop),
+                })),
+            },
+        }
+    }
+
+    fn parse_time(&mut self) -> Result<Option<(Time, bool)>, Box<dyn Error>> {
+        match self.read_year()? {
+            None => Ok(None),
+            Some(year) => {
+                let mut res = Time {
+                    year,
+                    month: 0,
+                    day: 0,
+                    hour: 0,
+                    minute: 0,
+                    utc_offset: None,
+                };
+                self.read_expected(b'-')?;
+                res.month = self.read_number(10)? as u8;
+                self.read_expected(b'-')?;
+                res.day = self.read_number(10)? as u8;
+                self.read_expected(b' ')?;
+                res.hour = self.read_number(10)? as u8;
+                self.read_expected(b':')?;
+                res.minute = self.read_number(10)? as u8;
+                match self.read()? {
+                    None | Some(b'\n') => Ok(Some((res, true))),
+                    Some(b',') => Ok(Some((res, false))),
+                    Some(b' ') => {
+                        let sign: i16 = match self.read()? {
+                            Some(b'-') => -1,
+                            Some(b'+') => 1,
+                            None => return Err(self.error("expected +/- but got EOF".to_string())),
+                            Some(x) => {
+                                return Err(
+                                    self.error(format!("expected +/- but got '{}'", x as char))
+                                )
+                            }
+                        };
+                        let hr_off = self.read_number(10)? as i16;
+                        let min_off = self.read_number(10)? as i16;
+                        res.utc_offset = Some(sign * ((hr_off * 60) + min_off));
+                        match self.read()? {
+                            None | Some(b'\n') => Ok(Some((res, true))),
+                            Some(b',') => Ok(Some((res, false))),
+                            Some(x) => {
+                                Err(self.error(format!("expected '\\n' but got '{}'", x as char)))
+                            }
+                        }
+                    }
+                    Some(x) => Err(self.error(format!(
+                        "expected newline, comma, or space, but got '{}'",
+                        x as char
+                    ))),
+                }
+            }
+        }
+    }
+
+    fn read_year(&mut self) -> Result<Option<u16>, Box<dyn Error>> {
+        match self.read()? {
+            None => Ok(None),
+            Some(digit) => {
+                let digit = self.parse_digit(digit)?;
+                let year = 1000 * digit + self.read_number(100)?;
+                Ok(Some(year))
+            }
+        }
+    }
+
+    fn read_expected(&mut self, expected: u8) -> Result<(), Box<dyn Error>> {
+        match self.read()? {
+            None => Err(self.error(format!("expected '{}' but got EOF", expected as char))),
+            Some(x) => {
+                if x == expected {
+                    Ok(())
+                } else {
+                    Err(self.error(format!(
+                        "expected '{}' but got '{}'",
+                        expected as char, x as char
+                    )))
+                }
+            }
+        }
+    }
+
+    fn read_number(&mut self, scale: u16) -> Result<u16, Box<dyn Error>> {
+        match self.read()? {
+            None => Err(self.error("expected a digit but got EOF".to_string())),
+            Some(digit) => {
+                let digit = self.parse_digit(digit)?;
+                match scale {
+                    1 => Ok(digit),
+                    _ => Ok(digit * scale + self.read_number(scale / 10)?),
+                }
+            }
+        }
+    }
+
     fn read(&mut self) -> io::Result<Option<u8>> {
         let mut buf = [0; 1];
         let len = self.reader.read(&mut buf)?;
@@ -73,134 +187,18 @@ impl<R: Read> Parser<R> {
         Ok(Some(c))
     }
 
+    fn parse_digit(&self, digit: u8) -> Result<u16, Box<dyn Error>> {
+        match digit {
+            b'0' | b'1' | b'2' | b'3' | b'4' | b'5' | b'6' | b'7' | b'8' | b'9' => {
+                Ok((digit - b'0') as u16)
+            }
+            x => Err(self.error(format!("expected a digit but got '{}'", x as char))),
+        }
+    }
+
     fn error(&self, message: String) -> Box<dyn Error> {
         let message = format!("line {}, col {}: {}", self.line, self.col, message);
         Box::new(ParseError { message })
-    }
-}
-
-fn parse_entry<R: Read>(parser: &mut Parser<R>) -> Result<Option<Entry>, Box<dyn Error>> {
-    match parse_time(parser)? {
-        None => Ok(None),
-        Some((start, true)) => Ok(Some(Entry {
-            start: start,
-            stop: None,
-        })),
-        Some((start, false)) => match (parse_time(parser))? {
-            None => Ok(Some(Entry {
-                start: start,
-                stop: None,
-            })),
-            Some((stop, _)) => Ok(Some(Entry {
-                start: start,
-                stop: Some(stop),
-            })),
-        },
-    }
-}
-
-fn parse_time<R: Read>(parser: &mut Parser<R>) -> Result<Option<(Time, bool)>, Box<dyn Error>> {
-    match read_year(parser)? {
-        None => Ok(None),
-        Some(year) => {
-            let mut res = Time {
-                year,
-                month: 0,
-                day: 0,
-                hour: 0,
-                minute: 0,
-                utc_offset: None,
-            };
-            read_expected(parser, b'-')?;
-            res.month = read_number(parser, 10)? as u8;
-            read_expected(parser, b'-')?;
-            res.day = read_number(parser, 10)? as u8;
-            read_expected(parser, b' ')?;
-            res.hour = read_number(parser, 10)? as u8;
-            read_expected(parser, b':')?;
-            res.minute = read_number(parser, 10)? as u8;
-            match parser.read()? {
-                None | Some(b'\n') => Ok(Some((res, true))),
-                Some(b',') => Ok(Some((res, false))),
-                Some(b' ') => {
-                    let sign: i16 = match parser.read()? {
-                        Some(b'-') => -1,
-                        Some(b'+') => 1,
-                        None => return Err(parser.error("expected +/- but got EOF".to_string())),
-                        Some(x) => {
-                            return Err(
-                                parser.error(format!("expected +/- but got '{}'", x as char))
-                            )
-                        }
-                    };
-                    let hr_off = read_number(parser, 10)? as i16;
-                    let min_off = read_number(parser, 10)? as i16;
-                    res.utc_offset = Some(sign * ((hr_off * 60) + min_off));
-                    match parser.read()? {
-                        None | Some(b'\n') => Ok(Some((res, true))),
-                        Some(b',') => Ok(Some((res, false))),
-                        Some(x) => {
-                            Err(parser.error(format!("expected '\\n' but got '{}'", x as char)))
-                        }
-                    }
-                }
-                Some(x) => Err(parser.error(format!(
-                    "expected newline, comma, or space, but got '{}'",
-                    x as char
-                ))),
-            }
-        }
-    }
-}
-
-fn read_expected<R: Read>(parser: &mut Parser<R>, expected: u8) -> Result<(), Box<dyn Error>> {
-    match parser.read()? {
-        None => Err(parser.error(format!("expected '{}' but got EOF", expected as char))),
-        Some(x) => {
-            if x == expected {
-                Ok(())
-            } else {
-                Err(parser.error(format!(
-                    "expected '{}' but got '{}'",
-                    expected as char, x as char
-                )))
-            }
-        }
-    }
-}
-
-fn read_number<R: Read>(parser: &mut Parser<R>, scale: u16) -> Result<u16, Box<dyn Error>> {
-    match parser.read()? {
-        None => Err(parser.error("expected a digit but got EOF".to_string())),
-        Some(digit) => match parse_digit(digit) {
-            Err(err) => Err(parser.error(err)),
-            Ok(digit) => match scale {
-                1 => Ok(digit),
-                _ => Ok(digit * scale + read_number(parser, scale / 10)?),
-            },
-        },
-    }
-}
-
-fn read_year<R: Read>(parser: &mut Parser<R>) -> Result<Option<u16>, Box<dyn Error>> {
-    match parser.read()? {
-        None => Ok(None),
-        Some(digit) => match parse_digit(digit) {
-            Err(err) => Err(parser.error(err)),
-            Ok(digit) => {
-                let year = 1000 * digit + read_number(parser, 100)?;
-                Ok(Some(year))
-            }
-        },
-    }
-}
-
-fn parse_digit(digit: u8) -> Result<u16, String> {
-    match digit {
-        b'0' | b'1' | b'2' | b'3' | b'4' | b'5' | b'6' | b'7' | b'8' | b'9' => {
-            Ok((digit - b'0') as u16)
-        }
-        x => Err(format!("expected a digit but got '{}'", x as char)),
     }
 }
 
@@ -213,17 +211,16 @@ mod tests {
     // - partial entry with/without comma
 
     use super::{parse_entries, Entry, Time};
-    use std::io::Cursor;
 
     #[test]
     fn test_empty() {
-        let actual = parse_entries(Cursor::new(b"")).unwrap();
+        let actual = parse_entries("".as_bytes()).unwrap();
         assert_eq!(Vec::<Entry>::new(), actual);
     }
 
     #[test]
     fn test_start_no_tz() {
-        let actual = parse_entries(Cursor::new(b"2020-01-02 12:34\n")).unwrap();
+        let actual = parse_entries("2020-01-02 12:34\n".as_bytes()).unwrap();
         assert_eq!(
             vec![Entry {
                 start: Time {
@@ -235,14 +232,14 @@ mod tests {
                     utc_offset: None
                 },
                 stop: None,
-            },],
+            }],
             actual
         );
     }
 
     #[test]
     fn test_start_with_neg_tz() {
-        let actual = parse_entries(Cursor::new(b"2020-01-02 12:34 -1001\n")).unwrap();
+        let actual = parse_entries("2020-01-02 12:34 -1001\n".as_bytes()).unwrap();
         assert_eq!(
             vec![Entry {
                 start: Time {
@@ -254,14 +251,14 @@ mod tests {
                     utc_offset: Some(-601)
                 },
                 stop: None,
-            },],
+            }],
             actual
         );
     }
 
     #[test]
     fn test_start_with_pos_tz() {
-        let actual = parse_entries(Cursor::new(b"2020-01-02 12:34 +1001\n")).unwrap();
+        let actual = parse_entries("2020-01-02 12:34 +1001\n".as_bytes()).unwrap();
         assert_eq!(
             vec![Entry {
                 start: Time {
@@ -273,7 +270,7 @@ mod tests {
                     utc_offset: Some(601)
                 },
                 stop: None,
-            },],
+            }],
             actual
         );
     }
@@ -300,14 +297,49 @@ mod tests {
                     minute: 34,
                     utc_offset: Some(-240)
                 }),
-            },],
+            }],
             actual
         );
     }
 
-    // test_single_entry_mixed_tz
+    #[test]
+    fn test_single_entry_mixed_tz() {
+        let actual =
+            parse_entries("2020-01-02 12:34 +1000,2020-01-02 13:34 -0400\n".as_bytes()).unwrap();
+        assert_eq!(
+            vec![Entry {
+                start: Time {
+                    year: 2020,
+                    month: 1,
+                    day: 2,
+                    hour: 12,
+                    minute: 34,
+                    utc_offset: Some(600)
+                },
+                stop: Some(Time {
+                    year: 2020,
+                    month: 1,
+                    day: 2,
+                    hour: 13,
+                    minute: 34,
+                    utc_offset: Some(-240)
+                }),
+            }],
+            actual
+        );
+    }
 
-    // test_several_entries
+    #[test]
+    fn test_several_entries() {
+        let actual = parse_entries(
+            "2020-01-02 12:34 +1000,2020-01-02 13:34 -0400\n\
+                           2020-01-03 09:00, 2020-01-03 10:30\n\
+                           2020-02-02 11:11, 2020-02-02 12:12 -0400\n"
+                .as_bytes(),
+        )
+        .unwrap();
+        assert_eq!(3, actual.len());
+    }
 
     // test_several_entries_last_is_partial
 }
