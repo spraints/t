5d9ab1cc1542b3a446020bdb6fb6b3fe364187b0
Matt Burke <spraints@gmail.com>
2020-07-28 07:11:35 -0400

    Time::now()

--- src/entry.rs
+++ src/entry.rs
@@ -1,4 +1,6 @@
-use std::fmt::{self, Formatter, Display};
+use chrono::prelude::Local;
+use chrono::{Datelike, Timelike};
+use std::fmt::{self, Display, Formatter};
 
 #[derive(Debug, PartialEq)]
 pub struct Entry {
@@ -33,15 +35,38 @@ pub struct Time {
     pub utc_offset: Option<i16>,
 }
 
+impl Time {
+    fn now() -> Self {
+        let time = Local::now();
+        let sec_offset = time.offset().local_minus_utc() as i16;
+        Time {
+            year: time.year() as u16,
+            month: time.month() as u8,
+            day: time.day() as u8,
+            hour: time.hour() as u8,
+            minute: time.minute() as u8,
+            utc_offset: Some(sec_offset / 60),
+        }
+    }
+}
+
 impl Display for Time {
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
         match self.utc_offset {
-            None => write!(f, "{:04}-{:02}-{:02} {:02}:{:02}", self.year, self.month, self.day, self.hour, self.minute),
+            None => write!(
+                f,
+                "{:04}-{:02}-{:02} {:02}:{:02}",
+                self.year, self.month, self.day, self.hour, self.minute
+            ),
             Some(off) => {
                 let (sign, off) = if off < 0 { ("-", -off) } else { ("+", off) };
                 let hr_off = off / 60;
                 let min_off = off % 60;
-            write!(f, "{:04}-{:02}-{:02} {:02}:{:02} {}{:02}{:02}", self.year, self.month, self.day, self.hour, self.minute, sign, hr_off,min_off)
+                write!(
+                    f,
+                    "{:04}-{:02}-{:02} {:02}:{:02} {}{:02}{:02}",
+                    self.year, self.month, self.day, self.hour, self.minute, sign, hr_off, min_off
+                )
             }
         }
     }
@@ -53,20 +78,41 @@ mod tests {
 
     #[test]
     fn test_time_format_no_tz() {
-        let time = Time{year: 2020, month: 6, day: 20, hour: 1, minute:7, utc_offset: None};
+        let time = Time {
+            year: 2020,
+            month: 6,
+            day: 20,
+            hour: 1,
+            minute: 7,
+            utc_offset: None,
+        };
         assert_eq!("2020-06-20 01:07", format!("{}", time));
     }
 
     #[test]
     fn test_time_format_with_tz() {
-        let time = Time{year: 2020, month: 6, day: 20, hour: 1, minute:7, utc_offset: Some(-123)};
+        let time = Time {
+            year: 2020,
+            month: 6,
+            day: 20,
+            hour: 1,
+            minute: 7,
+            utc_offset: Some(-123),
+        };
         assert_eq!("2020-06-20 01:07 -0203", format!("{}", time));
     }
 
     #[test]
     fn test_entry_format_with_start() {
-        let entry = Entry{
-            start:Time{year: 2020, month: 6, day: 20, hour: 1, minute:7, utc_offset: None},
+        let entry = Entry {
+            start: Time {
+                year: 2020,
+                month: 6,
+                day: 20,
+                hour: 1,
+                minute: 7,
+                utc_offset: None,
+            },
             stop: None,
         };
         assert_eq!("2020-06-20 01:07\n", format!("{}", entry));
@@ -74,17 +120,36 @@ mod tests {
 
     #[test]
     fn test_entry_format_with_start_and_stop() {
-        let entry = Entry{
-            start:Time{year: 2020, month: 6, day: 20, hour: 1, minute:7, utc_offset: None},
-            stop: Some(Time{year: 2020, month: 6, day: 20, hour: 1, minute:8, utc_offset: None}),
+        let entry = Entry {
+            start: Time {
+                year: 2020,
+                month: 6,
+                day: 20,
+                hour: 1,
+                minute: 7,
+                utc_offset: None,
+            },
+            stop: Some(Time {
+                year: 2020,
+                month: 6,
+                day: 20,
+                hour: 1,
+                minute: 8,
+                utc_offset: None,
+            }),
         };
         assert_eq!("2020-06-20 01:07,2020-06-20 01:08\n", format!("{}", entry));
     }
 
-    // #[test]
-    // fn test_now() {
-    //     panic!("TODO: Entry::now()");
-    // }
+    // I wish there was a better way to set this. :/
+    const TEST_TZ: i16 = -4 * 60;
+
+    #[test]
+    fn test_now() {
+        let time = Time::now();
+        assert!(time.utc_offset.is_some());
+        assert_eq!(Some(TEST_TZ), time.utc_offset);
+    }
 
     // #[test]
     // fn test_minutes() {
