16a86903a6f08364da76f27a1f3d7c95bddb3c09
Matt Burke <spraints@gmail.com>
2020-08-01 17:53:47 -0400

    sketch a benchmark

---
+++ bench.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+
+if [ ! -f "$T_DATA_FILE" ]; then
+  echo "T_DATA_FILE must be set to a real file"
+  exit 1
+fi
+
+#TODO ruby -Ilib -e 'PARSE THE FILE'
+#TODO ruby -Ilib -e 'PARSE THE FILE AND ADD UP ALL THE TIME'
+
+#TODO rust version of PARSE THE FILE
+#TODO rust version of PARSE THE FILE AND ADD UP ALL THE TIME
