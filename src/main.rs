d7adba8694a00218c6b96ace1598d3d5a168e106
Matt Burke <spraints@gmail.com>
2020-08-02 21:13:56 -0400

    't edit' in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::os::unix::process::CommandExt;
+
 fn main() {
     // Skip over the program name.
     let mut args = std::env::args().skip(1);
@@ -6,7 +8,7 @@ fn main() {
         Some(cmd) => match cmd.as_str() {
             "start" => (),
             "stop" => (),
-            "edit" => (),
+            "edit" => cmd_edit(args),
             "status" => (),
             "today" => (),
             "week" => (),
@@ -30,5 +32,12 @@ fn unknown_command(cmd: &str) -> ! {
 
 fn usage() -> ! {
     eprintln!("A command (start, stop, edit) or query (status, today, week, all, punchcard, days, csv, svg, pto, short, path) is required.");
+    std::process::exit(1)
+}
+
+fn cmd_edit(_: impl Iterator) -> ! {
+    let editor = std::env::var("EDITOR").unwrap();
+    let path = std::env::var("T_DATA_FILE").unwrap();
+    eprintln!("error: {}", std::process::Command::new(editor).arg(path).exec());
     std::process::exit(1)
 }
