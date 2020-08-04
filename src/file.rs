8931080ca21d49088b36e4b3a7fb8f302ba1710d
Matt Burke <spraints@gmail.com>
2020-08-03 23:16:31 -0400

    Read less than the whole file when looking at this week

--- src/file.rs
+++ src/file.rs
@@ -2,24 +2,50 @@ use crate::entry::Entry;
 use crate::parser::parse_entries;
 use std::error::Error;
 use std::fs::File;
+use std::io::{Read, Seek, SeekFrom};
+
+const APPROX_LINE_LENGTH_FOR_SEEK: u64 = 50;
 
 pub fn t_data_file() -> String {
     std::env::var("T_DATA_FILE").unwrap()
 }
 
 pub fn read_entries() -> Result<Vec<Entry>, Box<dyn Error>> {
-    match File::open(t_data_file()) {
-        Err(_) => Ok(vec![]),
-        Ok(f) => parse_entries(f),
-    }
+    _read_entries(|f| Ok(f))
 }
 
 pub fn read_last_entry() -> Result<Option<Entry>, Box<dyn Error>> {
-    // TODO - seek close to the end
-    Ok(read_entries()?.into_iter().last())
+    Ok(read_last_entries(1)?.into_iter().last())
 }
 
-pub fn read_last_entries(_: usize) -> Result<Vec<Entry>, Box<dyn Error>> {
-    // TODO
-    read_entries()
+pub fn read_last_entries(n: u64) -> Result<Vec<Entry>, Box<dyn Error>> {
+    _read_entries(|mut f| {
+        let len = f.metadata()?.len();
+        let off = n * APPROX_LINE_LENGTH_FOR_SEEK;
+        if off < len {
+            f.seek(SeekFrom::Start(len - off))?;
+            read_to(&mut f, b'\n')?;
+        }
+        Ok(f)
+    })
+}
+
+fn _read_entries<F>(seek: F) -> Result<Vec<Entry>, Box<dyn Error>>
+where
+    F: Fn(File) -> Result<File, Box<dyn Error>>,
+{
+    match File::open(t_data_file()) {
+        Err(_) => Ok(vec![]),
+        Ok(f) => parse_entries(seek(f)?),
+    }
+}
+
+fn read_to(f: &mut File, c: u8) -> Result<(), Box<dyn Error>> {
+    let mut buf = [0; 1];
+    loop {
+        let res = f.read(&mut buf)?;
+        if res == 0 || buf[0] == c {
+            return Ok(());
+        }
+    }
 }
