2a3483a32a23a51c14ecfc996b92fb42f9a80080
Matt Burke <spraints@gmail.com>
2024-08-14 22:33:59 -0400

    yay

--- src/web.rs
+++ src/web.rs
@@ -1,3 +1,14 @@
+use rocket::{get, routes};
+
 pub async fn web_main() {
-    rocket::build().launch().await.unwrap();
+    rocket::build()
+        .mount("/", routes![yay])
+        .launch()
+        .await
+        .unwrap();
+}
+
+#[get("/")]
+fn yay() -> &'static str {
+    "yay\r\n"
 }
